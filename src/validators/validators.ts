bde3882aaa625d0e7d08f33d7dc017816e2b8f6e
Mikołaj Urbański <m.urbanski@goldensubmarine.com>
2023-11-29 13:22:22 +0100

    Create validators functions and tests to them

---
+++ src/validators/validators.ts
@@ -0,0 +1,19 @@
+import {type} from "os";
+
+export const notEmpty = (entry: string): boolean => {
+    if(typeof entry === null || typeof entry === "undefined") return false;
+
+    return entry.trim().length > 0;
+}
+
+export const isNumber = (entry: string): boolean => {
+    return !isNaN(Number(entry));
+}
+
+export const isMoreThanZero = (entry: string): boolean => {
+    return isNumber(entry) && parseFloat(entry) > 0;
+}
+
+export const isPositiveInteger = (entry: string): boolean => {
+    return isMoreThanZero(entry) && Number.isInteger(Number(entry));
+}
