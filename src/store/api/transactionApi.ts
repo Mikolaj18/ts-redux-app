1504a322aa53e3600b073e2df9c08a3399f5ceed
Mikołaj Urbański <m.urbanski@goldensubmarine.com>
2023-11-29 14:52:37 +0100

    Create endpoints in transactionApi

--- src/store/api/transactionApi.ts
+++ src/store/api/transactionApi.ts
@@ -1,6 +1,7 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query";
 import {TTransaction} from "../../types/Transactions";
 
+type TPostTransaction = Omit<TTransaction, "id">;
 export const transactionApi = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: 'http://localhost:3005',
@@ -9,6 +10,18 @@ export const transactionApi = createApi({
         fetchTransactions: builder.query<TTransaction[], void>({
             query: () => ({
                 url: '/transactions',
+            }),
+        }),
+        addTransaction: builder.mutation<TTransaction, TPostTransaction>({
+            query: (body) => ({
+                url: '/transactions',
+                body,
+            }),
+        }),
+        removeTransaction: builder.mutation<void, TTransaction>({
+            query: (transaction) => ({
+               url: `/transactions/${transaction.id}`,
+               method: 'DELETE',
             }),
         }),
     }),
